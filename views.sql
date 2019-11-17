d6a600109bdd3f1c07efe1d7af654b1ff2155b65
Thiago Lima <44410746+thiagolima08@users.noreply.github.com>
2019-11-17 11:05:21 -0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,13 +1,13 @@
 CREATE VIEW condutor_com_pontos AS
 (
-	SELECT con.idCadastro,con.nome "Nome do condutor",con.idcategoria_cnh "Categoria", date_part('YEAR',mul.dataInfracao) "Ano da infração", inf.pontos "Pontos"
+	SELECT con.idCadastro,con.nome "Nome do condutor",con.idcategoriacnh "Categoria", date_part('YEAR',mul.dataInfracao) "Ano da infração", inf.pontos "Pontos"
 	FROM condutor con JOIN multa mul ON con.idCadastro = mul.idCondutor JOIN infracao inf ON inf.idinfracao = mul.idinfracao
 	WHERE inf.pontos > 0
 );
 
 CREATE VIEW veiculos_proprietarios AS
 (
-	SELECT vei.renavam Renavam,vei.placa Placa,con.nome Proprietário,mod.denominacao Modelo,mar.nome Marca,cid.nome Cidade,est.nome Estado,tip.descricao Tipo
+	SELECT vei.renavam "Renavam",vei.placa Placa,con.nome "Proprietário",mod.denominacao "Modelo",mar.nome "Marca",cid.nome "Cidade",est.nome "Estado",tip.descricao "Tipo"
 	FROM veiculo vei JOIN condutor con ON vei.idProprietario = con.idCadastro JOIN modelo mod ON mod.idModelo=vei.idModelo JOIN marca mar ON mar.idMarca=mod.idMarca JOIN cidade cid ON cid.idCidade=vei.idCidade JOIN estado est ON est.uf=cid.uf JOIN tipo tip ON tip.idTipo=mod.idTipo
 );
 
