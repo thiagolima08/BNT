f49af6a2539e4aff092f98d38b168bc0668e8569
Ediko Dominike <ediko.dominike@gmail.com>
2019-11-26 16:26:26 -0300

    ADD gerar_renavam

--- function.sql
+++ function.sql
@@ -42,3 +42,42 @@ BEGIN
     ORDER BY "Data de Venda";
 END; $$
 LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS gerar_dv;
+CREATE OR REPLACE FUNCTION gerar_dv (num CHAR(10))
+RETURNS CHAR(1)
+AS $$
+DECLARE
+    somador  INTEGER := 0;
+    n        INTEGER := 0;
+    m        INTEGER := 0;
+    contador INTEGER := 1;
+BEGIN
+    LOOP 
+        EXIT WHEN contador > 10;
+        n := CAST(SUBSTRING(num, contador, 1) AS INTEGER);
+        m := CAST(SUBSTRING('3298765432', contador, 1) AS INTEGER);
+        somador := somador + (n * m);
+        contador := contador + 1 ; 
+    END LOOP ; 
+
+    RETURN CAST(((somador * 10) % 11) % 10 AS CHAR(1));
+END; $$
+LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS gerar_renavam;
+CREATE OR REPLACE FUNCTION gerar_renavam (pla CHAR(7))
+RETURNS CHAR(13)
+AS $$
+DECLARE
+    acumulador  char(10);
+BEGIN
+    acumulador := CONCAT(
+        CAST(ASCII(UPPER(SUBSTRING(pla, 1, 1))) AS CHAR(2)),
+        CAST(ASCII(UPPER(SUBSTRING(pla, 2, 1))) AS CHAR(2)),
+        CAST(ASCII(UPPER(SUBSTRING(pla, 3, 1))) AS CHAR(2)),
+        SUBSTRING(PLA, 4, 4)
+    );
+    RETURN CONCAT(acumulador, gerar_dv(acumulador));
+END; $$
+LANGUAGE plpgsql;
