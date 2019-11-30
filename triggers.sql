8e58b250f8384c4b7febf9a1a6c17d509c971ba8
Ediko Dominike <ediko.dominike@gmail.com>
2019-11-30 17:01:27 -0300

    ADD trigger de multa

--- triggers.sql
+++ triggers.sql
@@ -10,3 +10,25 @@ CREATE TRIGGER tgInsertVeiculo
 BEFORE INSERT ON veiculo
 FOR EACH ROW
 EXECUTE PROCEDURE insertVeiculo();
+
+
+CREATE OR REPLACE FUNCTION insertMulta()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (NEW.idInfracao) <= 6
+       OR
+       (SELECT sum(inf.pontos) FROM multa as mul LEFT JOIN infracao as inf on mul.idInfracao = inf.idInfracao WHERE mul.renavam = NEW.renavam AND mul.dataInfracao <= NEW.dataInfracao AND mul.dataInfracao >= date(NEW.dataInfracao - interval '1 year')) >= 20
+    THEN
+        UPDATE condutor as cond
+        SET situacaoCNH = 'S'
+        WHERE cond.idCadastro = NEW.idCondutor;
+    END IF;
+
+    RETURN NEW;
+END; $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tgInsertMulta
+BEFORE INSERT ON multa
+FOR EACH ROW
+EXECUTE PROCEDURE insertMulta();
